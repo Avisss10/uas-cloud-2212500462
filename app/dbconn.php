3e4bb15e068ef0bd7426d0ce38f3f322831bf46f
Avisss10 <stevisss10@gmail.com>
2025-07-11 13:10:02 +0700

    first commit

---
+++ app/dbconn.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "db"; //or you could use hostname (service name if using docker-compose)
+$username = "root";
+$password = "";
+$dbname = "2212500462";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
